1deac9a7d1973b7e73982c7db78053fc0bf5cfd4
sjgeyer <sjgeyer@comcast.net>
2018-03-30 09:12:23 -0700

    fix redirect

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,8 +1,8 @@
 'use strict';
 
-page('/', ctx => app.Book.fetchAll(app.bookView.initListView));
 page('/books/new', ctx => app.bookView.initFormView());
 page('/books/:book_id/update', app.Book.fetchOne, app.bookView.initUpdateFormPage);
 page('/books/:book_id', app.Book.fetchOne, app.bookView.initDetailView);
+page('/', ctx => app.Book.fetchAll(app.bookView.initListView));
 
 page();
